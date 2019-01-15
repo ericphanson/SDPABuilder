50dd99b100e7d888289131d3a6c4ae3d07205f83
jvielma <aa@aa.com>
2019-01-15 18:44:16 -0500

    Staticly linked build

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -9,24 +9,58 @@ version = v"7.3.8"
 sources = [
     "https://sourceforge.net/projects/sdpa/files/sdpa/sdpa_7.3.8.tar.gz" =>
     "c7541333da2f0bb2d18e90dbf758ac7cc099f3f7da3f256b284b0725f96d4117",
-
+    "./bundled"
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd sdpa-7.3.8/
-./configure --prefix=$prefix --host=$target --with-blas="-L${prefix}/lib -lopenblas" --with-lapack="-L${prefix}/lib -lopenblas"
+update_configure_scripts
+
+for path in ${LD_LIBRARY_PATH//:/ }; do
+    for file in $(ls $path/*.la); do
+        echo "$file"
+        baddir=$(sed -n "s|libdir=||p" $file)
+        sed -i~ -e "s|$baddir|'$path'|g" $file
+    done
+done
+if [ $target = "x86_64-apple-darwin14" ]; then
+  # seems static linking requires apple's ar
+  export AR=/opt/x86_64-apple-darwin14/bin/x86_64-apple-darwin14-ar
+fi
+
+patch -p1 < $WORKSPACE/srcdir/patches/shared.diff
+mv configure.in configure.ac
+patch -p1 < $WORKSPACE/srcdir/patches/lt_init.diff
+autoreconf -i
+
+
+./configure --prefix=$prefix --host=${target}  lt_cv_deplibs_check_method=pass_all \
+--with-blas="-L${prefix}/lib -lcoinblas -lgfortran -lcoinmumps -lcoinmetis" \
+--with-lapack="-L${prefix}/lib -lcoinlapack -lcoinmumps -lcoinmetis" \
+--with-mumps-libs="-L${prefix}/lib -lcoinmumps -lcoinmetis" --with-mumps-include="-I$prefix/include/coin/ThirdParty"
+
 make
 make install
-
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:i686, :glibc)
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    MacOS(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64)
 ]
+platforms = expand_gcc_versions(platforms)
+# To fix gcc4 bug in Windows
+# platforms = setdiff(platforms, [Windows(:x86_64, compiler_abi=CompilerABI(:gcc4)), Windows(:i686, compiler_abi=CompilerABI(:gcc4))])
+push!(platforms, Windows(:i686,compiler_abi=CompilerABI(:gcc6)))
+push!(platforms, Windows(:x86_64,compiler_abi=CompilerABI(:gcc6)))
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -35,9 +69,12 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/JuliaLinearAlgebra/OpenBLASBuilder/releases/download/v0.3.0-1/build_OpenBLAS.v0.3.0.jl"
+    "https://github.com/juan-pablo-vielma/COINBLASBuilder/releases/download/v1.4.6-1-static/build_COINBLASBuilder.v1.4.6.jl",
+    "https://github.com/juan-pablo-vielma/COINLapackBuilder/releases/download/v1.5.6-1-static/build_COINLapackBuilder.v1.5.6.jl",
+    "https://github.com/juan-pablo-vielma/COINMetisBuilder/releases/download/v1.3.5-1-static/build_COINMetisBuilder.v1.3.5.jl",
+    "https://github.com/juan-pablo-vielma/COINMumpsBuilder/releases/download/v1.6.0-1-static/build_COINMumpsBuilder.v1.6.0.jl"
+
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
